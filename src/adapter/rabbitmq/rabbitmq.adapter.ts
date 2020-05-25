31ab53334ae7a6654b1fb8c9e870b6fe5e0359b3
PrateepUC <prateep@ucconnect.co.th>
2020-05-25 15:02:55 +0700

    config env

--- src/adapter/rabbitmq/rabbitmq.adapter.ts
+++ src/adapter/rabbitmq/rabbitmq.adapter.ts
@@ -1,23 +1,11 @@
 import { IRabbitmqAdapter } from './interface/adapter.interface'
 import {
-    from,
     Observable,
     of,
-    throwError,
 } from 'rxjs'
 import { IConfig } from '../../common/interface/config.interface'
 import * as amqp from 'amqplib/callback_api'
-import {
-    catchError,
-    concatMap,
-    map,
-    tap,
-} from 'rxjs/operators'
-import * as _ from 'lodash'
-import {
-    HttpException,
-    HttpStatus,
-} from '@nestjs/common'
+import { concatMap } from 'rxjs/operators'
 import { IMessageLog } from './interface/schema.interface'
 
 export class RabbitmqAdapter implements IRabbitmqAdapter {
