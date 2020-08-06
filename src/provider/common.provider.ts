034c582b398e3b34fcabd3c10cc9dd66e9f7071e
jakkraphan1996 <jakkraphan@ucconnect.co.th>
2020-08-06 20:51:16 +0700

    process.env username, password log send message env docker file

--- src/provider/common.provider.ts
+++ src/provider/common.provider.ts
@@ -9,7 +9,6 @@ export const commonProviders: Provider[] = [
         provide: providerNames.CONFIG,
         useFactory: (): IConfig => {
             const configApp = Config.util.toObject()
-            // process.env.AMQP_HOST
             if (!_.isEmpty(process.env.AMQP_HOST)) {
                 configApp.amqp.host = process.env.AMQP_HOST
             }
@@ -19,6 +18,14 @@ export const commonProviders: Provider[] = [
 
             if(!_.isEmpty(process.env.NODE_PORT)) {
                 configApp.application.port = process.env.NODE_PORT
+            }
+
+            if(!_.isEmpty(process.env.AMQP_USERNAME)) {
+                configApp.amqp.username = process.env.AMQP_USERNAME
+            }
+
+            if(!_.isEmpty(process.env.AMQP_PASSWORD)) {
+                configApp.amqp.password = process.env.AMQP_PASSWORD
             }
             return configApp
         }
